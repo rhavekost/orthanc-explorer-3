b2a6acd1e7e4d4fd9a6a004c2b086825bca976e0
kanban-gate <rob.havekost@gmail.com>
2026-07-22 22:19:56 -0500

    gate: commit pending worker changes

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,5 +1,18 @@
 import "@testing-library/jest-dom";
 
+if (typeof Blob.prototype.arrayBuffer !== "function") {
+  Object.defineProperty(Blob.prototype, "arrayBuffer", {
+    value: function arrayBuffer(this: Blob): Promise<ArrayBuffer> {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onerror = () => reject(reader.error);
+        reader.onload = () => resolve(reader.result as ArrayBuffer);
+        reader.readAsArrayBuffer(this);
+      });
+    },
+  });
+}
+
 Object.defineProperty(window, "matchMedia", {
   writable: true,
   value: (query: string) => ({
